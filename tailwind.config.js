96fcfea0cf44b021ee1bea0593ac590f60eeeaf9
LordCodex <adenirandaniel565@gmail.com>
2024-02-07 00:06:33 +0100

    add tailwind styling and install axios

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    `./src/**/*.{js,jsx,ts,tsx}`,
+    `./src/components/**/*.{js,jsx,ts,tsx}`,
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  screens: {
+    1440: "1440px"
+  },
+}
